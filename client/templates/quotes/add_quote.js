2b620ca13c2980e87cbf67dd7d8bac41e782b733
Nick Riebeek <nick.riebeek@gmail.com>
2015-01-13 21:44:31 -0500

    some updates to form submission

--- client/templates/quotes/add_quote.js
+++ client/templates/quotes/add_quote.js
@@ -1,6 +1,21 @@
+Template.name.rendered = function () {
+  $('#addQuote').parsley({trigger: 'change'});
+}
+
 Template.addQuote.events({
-	'submit': function(e) {
-		e.preventDefault();
-		alert('bob');
+  'submit form': function(e) {
+    e.preventDefault();
+
+    var quote = {
+      quote: $(e.target).find('[name=quote]').val(),
+      author: $(e.target).find('[name=author]').val()
+    };
+
+    Meteor.call('quoteInsert', quote, function(error, result) {    
+    	if (error)        
+    		throw error;     
+  		
+    	Router.go('viewQuote');      
+    });
   }
 });
