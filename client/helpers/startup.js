1ad54f10efe4c0474d4b48e2970f3277a8d4c9da
Nick Riebeek <nick.riebeek@gmail.com>
2015-01-15 18:11:02 -0500

    configured toastr

--- client/helpers/startup.js
+++ client/helpers/startup.js
@@ -1,3 +1,11 @@
 Meteor.startup(function () {
+
+	// set-up any toaster option over-rides
+  toastr.options = {
+  	"positionClass": "toast-bottom-right"
+  }
+
+  // get the first quote to display 
+  // when the app loads
   getQuote();
 });
