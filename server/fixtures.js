42f6f0a0ec8693f7baf1f35e555e7bca94ae7a3e
Nick Riebeek <nick.riebeek@gmail.com>
2015-01-15 22:03:49 -0500

    add more default quotes

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,15 +1,227 @@
 // Fixture data
 if (Quotes.find().count() === 0) {
 	Quotes.insert({
-		quote: 'Whatever the mind of man can conceive and believe, it can achieve.',
-		author: 'Napoleon Hill'
+		author: "Napoleon Hill",
+		quote: "Whatever the mind of man can conceive and believe, it can achieve."
 	});
 	Quotes.insert({
-		quote: 'Strive not to be a success, but rather to be of value.',
-		author: 'Albert Einstein'
+		author: "Albert Einstein",
+		quote: "Strive not to be a success, but rather to be of value."
 	});
 	Quotes.insert({
-		quote: 'Two roads diverged in a wood, and I—I took the one less traveled by, And that has made all the difference.',
-		author: 'Robert Frost'
+		author: "Horace Silver",
+		quote: "We open our minds, stretch forth, take chances and venture out musically to arrive at something new and different."
+	});
+	Quotes.insert({
+		author: "Horace Silver",
+		quote: "Jazz is not background music. You must concentrate in order to get the most out of it. You must absorb it."
+	});
+	Quotes.insert({
+		author: "Carly Simon",
+		quote: "You usually can't tell what's inspiring until you look back on it."
+	});
+	Quotes.insert({
+		author: "Nina Simone",
+		quote: "Music is a gift and a burden I've had since I can remember who I was."
+	});
+	Quotes.insert({
+		author: "Nina Simone",
+		quote: "Jazz is not just music, it's a way of life, it's a way of being, a way of thinking."
+	});
+	Quotes.insert({
+		author: "Zoot Sims",
+		quote: "As long as you've got your horn in your mouth, you're developing."
+	});
+	Quotes.insert({
+		author: "Homer Simpson",
+		quote: "Ahhh - those Jazz guys are just makin' that stuff up!"
+	});
+	Quotes.insert({
+		author: "Frank Sinatra",
+		quote: "I'm gonna live till I die."
+	});
+	Quotes.insert({
+		author: "Frank Sinatra",
+		quote: "You gotta love livin', baby, 'cause dyin' is a pain in the ass."
+	});
+	Quotes.insert({
+		author: "John Coltrane",
+		quote: "All of the technique doesn't matter... only if the feeling is right."
+	});
+	Quotes.insert({
+		author: "John Coltrane",
+		quote: "The first time I heard Bird play, it hit me right between the eyes."
+	});
+	Quotes.insert({
+		author: "John Coltrane",
+		quote: "Sometimes I think I was making music through the wrong end of a magnifying glass."
+	});
+	Quotes.insert({
+		author: "John Coltrane",
+		quote: "Through Dizzy and Bird I began to learn about musical structures and the more theoretical aspects of music."
+	});
+	Quotes.insert({
+		author: "John Coltrane",
+		quote: "When you begin to see the possibilities of music, you desire to do something really good for people."
+	});
+	Quotes.insert({
+		author: "John Coltrane",
+		quote: "Being so inescapably a part of it, I'll never know what the listener gets, what the listener feels and that's too bad."
+	});
+	Quotes.insert({
+		author: "Eddie Condon",
+		quote: "Beiderbecke took out a silver cornet, put it to his lips and blew a phrase. The sound came out like a girl saying 'yes.'"
+	});
+	Quotes.insert({
+		author: "Ray Conniff",
+		quote: "If you believe in your art and you love what you do, that energy will go out and people will respond."
+	});
+	Quotes.insert({
+		author: "Chick Corea",
+		quote: "Only play what you hear. If you don't hear anything, don't play anything."
+	});
+	Quotes.insert({
+		author: "Chick Corea",
+		quote: "Art is a subject that is inundated with opinions. In fact, that's all it is about is opinions."
+	});
+	Quotes.insert({
+		author: "Chick Corea",
+		quote: "The sound of the orchestra is one of the most magnificent musical sounds that has ever existed."
+	});
+	Quotes.insert({
+		author: "Chick Corea",
+		quote: "The fact that an artist is experiencing the joy of creation makes me feel like we're in it together."
+	});
+	Quotes.insert({
+		author: "Jim Croce",
+		quote: "If you dig it, do it. If you dig it a lot, do it twice."
+	});
+	Quotes.insert({
+		author: "Salvador Dali",
+		quote: "There is only 1 difference between a madman and me. I am not mad."
+	});
+	Quotes.insert({
+		author: "Salvador Dali",
+		quote: "Have no fear of perfection. You'll never reach it."
+	});
+	Quotes.insert({
+		author: "Bobby Darin",
+		quote: "It isn't true that you live only once. You only die once. You live lots of times if you know how."
+	});
+	Quotes.insert({
+		author: "Debbie Davies",
+		quote: "When you are so strongly drawn to music, you can't 'not' be a musician."
+	});
+	Quotes.insert({
+		author: "Miles Davis",
+		quote: "If you're trying to be hip, be hip."
+	});
+	Quotes.insert({
+		author: "Miles Davis",
+		quote: "Music and life are all about style."
+	});
+	Quotes.insert({
+		author: "Miles Davis",
+		quote: "Do not fear mistakes, there are none."
+	});
+	Quotes.insert({
+		author: "Miles Davis",
+		quote: "Always look ahead, but never look back."
+	});
+	Quotes.insert({
+		author: "Miles Davis",
+		quote: "I'll play it first and tell you what it is later."
+	});
+	Quotes.insert({
+		author: "Miles Davis",
+		quote: "Play what you know and then play above that."
+	});
+	Quotes.insert({
+		author: "Miles Davis",
+		quote: "Don't play what's there, play what's not there."
+	});
+	Quotes.insert({
+		author: "Miles Davis",
+		quote: "To keep creating you have to be about change."
+	});
+	Quotes.insert({
+		author: "Miles Davis",
+		quote: "If you understood everything I say, you'd be me!"
+	});
+	Quotes.insert({
+		author: "Miles Davis",
+		quote: "It's not the notes you play, it's the notes you don't play."
+	});
+	Quotes.insert({
+		author: "Miles Davis",
+		quote: "I can tell whether a person can play just by the way he stands."
+	});
+	Quotes.insert({
+		author: "Miles Davis",
+		quote: "You know why I quit playing ballads? Cause I love playing ballads."
+	});
+	Quotes.insert({
+		author: "Miles Davis",
+		quote: "Sometimes you have to play a long time to be able to play like yourself."
+	});
+	Quotes.insert({
+		author: "Miles Davis",
+		quote: "I don't care if a dude is purple with green breath as long as he can swing."
+	});
+	Quotes.insert({
+		author: "Miles Davis",
+		quote: "I really liked Wynton when I first met him. He's still a nice young man, only confused."
+	});
+	Quotes.insert({
+		author: "Miles Davis",
+		quote: "You should never be comfortable, man. Being comfortable fouled up a lot of musicians."
+	});
+	Quotes.insert({
+		author: "Miles Davis",
+		quote: "Bebop was about change, about evolution. It wasn't about standing still and being safe."
+	});
+	Quotes.insert({
+		author: "Miles Davis",
+		quote: "If you got up on the bandstand at Minton's and couldn't play, you might get your ass kicked."
+	});
+	Quotes.insert({
+		author: "Miles Davis",
+		quote: "So What or Kind of Blue were done in that era, the right hour, the right day. It's over; it's on the record."
+	});
+	Quotes.insert({
+		author: "Miles Davis",
+		quote: "I never thought Jazz was meant to be a museum piece like other dead things once considered artistic."
+	});
+	Quotes.insert({
+		author: "Miles Davis",
+		quote: "I'm always thinking about creating. My future starts when I wake up in the morning and see the light. Then I'm grateful."
+	});
+	Quotes.insert({
+		author: "Paul Desmond",
+		quote: "I was unfashionable before anyone knew who I was."
+	});
+	Quotes.insert({
+		author: "Paul Desmond",
+		quote: "I think I had it in the back of my mind that I wanted to sound like a dry martini."
+	});
+	Quotes.insert({
+		author: "Paul Desmond",
+		quote: "I would like to thank my father who discouraged me from playing the violin at an early age."
+	});
+	Quotes.insert({
+		author: "Paul Desmond",
+		quote: "I have won several prizes as the world's slowest alto player and a special award in 1961 for quietness."
+	});
+	Quotes.insert({
+		author: "Paul Desmond",
+		quote: "Qualities in music I consider most important are beauty, simplicity, originality, discrimination and sincerity."
+	});
+	Quotes.insert({
+		author: "Bo Diddley",
+		quote: "I thank you in advance for the great round of applause I'm about to get."
+	});
+	Quotes.insert({
+		author: "Eric Dolphy",
+		quote: "Trying to play the new concept with a outward bound feeling."
 	});
 }
